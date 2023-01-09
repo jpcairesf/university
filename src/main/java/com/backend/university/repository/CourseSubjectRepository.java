f3db667b6a653ae01ddd2ec7d411d8242a93b69c
Joao Caires <jpcairesf@gmail.com>
2023-01-09 20:23:37 -0300

    CourseSubject Repository

--- src/main/java/com/backend/university/repository/CourseSubjectRepository.java
+++ src/main/java/com/backend/university/repository/CourseSubjectRepository.java
@@ -4,9 +4,17 @@ import com.backend.university.domain.CourseSubject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseSubjectRepository extends JpaRepository<CourseSubject, Long> {
 
+    List<CourseSubject> findByCourseName(String courseName);
 
+    List<CourseSubject> findByCourseNameAndRequired(String courseName, boolean required);
+
+    List<CourseSubject> findByCourseNameAndSemester(String courseName, int semester);
+
+    List<CourseSubject> findBySubjectCode(String code);
 
 }
