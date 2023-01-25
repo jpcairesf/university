527a89738ac9b98e0a61f3e8ae435fff89fa3ed6
Joao Caires <jpcairesf@gmail.com>
2023-01-24 21:56:43 -0300

    Course controller

---
+++ src/main/java/com/backend/university/course/controller/CourseController.java
@@ -0,0 +1,47 @@
+package com.backend.university.course.controller;
+
+import com.backend.university.course.dto.CourseInputDTO;
+import com.backend.university.course.dto.CourseOutputDTO;
+import com.backend.university.course.dto.CourseUpdateDTO;
+import com.backend.university.course.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/course")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CourseController {
+
+    private final CourseService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseOutputDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseOutputDTO> create(@RequestBody CourseInputDTO input) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(input));
+    }
+
+    @PutMapping
+    public ResponseEntity<CourseOutputDTO> update(@RequestBody CourseUpdateDTO update) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(update));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+}
